20b9d90f217863626b7611d8a266a35176e81bd3
asters1 <gj744559140@gmail.com>
2023-05-07 16:54:51 +0800

    更新

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
-	f, _ := os.Open("./index.list")
+	//	f, _ := os.Open("./index.list")
+	//	f, _ := os.Open("./77.m3u8")
+	f, _ := os.Open("./live.m3u8")
 
 	//	fts,_:=io.ReadAll(f)
 	a, e := parse.Parse(f)
--- parse/mym3u8.go
+++ parse/mym3u8.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -14,8 +15,8 @@ type M3u8 struct {
 	TimeMax int
 
 	PlayList   []string
-	TimeList   []float32
-	TimeLength float32
+	TimeList   []float64
+	TimeLength float64
 	Key        Key
 }
 type Key struct {
@@ -25,6 +26,9 @@ type Key struct {
 	URI    string
 	IV     string
 }
+
+// 用于从一行中提取“键=值”参数的正则表达式模式
+var linePattern = regexp.MustCompile(`([a-zA-Z-]+)=("[^"]+"|[^",]+)`)
 
 func Parse(reader io.Reader) (*M3u8, error) {
 	s := bufio.NewScanner(reader)
@@ -48,18 +52,69 @@ func Parse(reader io.Reader) (*M3u8, error) {
 			continue
 		case strings.HasPrefix(line, "#EXT-X-VERSION:"):
 			m3u8.Version, _ = strconv.Atoi(line[15:])
+		case strings.HasPrefix(line, "#EXT-X-KEY"):
+
+			params := ParseLineParameters(line)
+			if len(params) == 0 {
+				return nil, fmt.Errorf("无效的 EXT-X-KEY: %s, 在第 %d 行", line, i+1)
+			}
+			if params["METHOD"] != "" {
+				m3u8.Key.METHOD = params["METHOD"]
+				if m3u8.Key.METHOD == "NONE" {
+					m3u8.Key.URI = ""
+					m3u8.Key.IV = ""
+				} else {
+					m3u8.Key.URI = params["URI"]
+					m3u8.Key.IV = params["IV"]
+				}
+			} else {
+				m3u8.Key.METHOD = "NONE"
+				m3u8.Key.URI = ""
+				m3u8.Key.IV = ""
+			}
 			continue
 		case strings.HasPrefix(line, "#EXT-X-TARGETDURATION:"):
 			m3u8.TimeMax, _ = strconv.Atoi(line[22:])
+			continue
 		case line == "#EXT-X-ENDLIST":
 			m3u8.IsLive = false
+		case strings.HasPrefix(line, "#EXTINF:"):
+			//			fmt.Println(line[len(line)-1:])
+			if line[len(line)-1:] == "," {
+				line = line[:len(line)-1]
+			}
+			//			fmt.Println(line[8:])
+			ftime, _ := strconv.ParseFloat(line[8:], 64)
+			//			fmt.Println(ftime)
+			m3u8.TimeList = append(m3u8.TimeList, ftime)
+			continue
+		case !strings.HasPrefix(line, "#"):
+			m3u8.PlayList = append(m3u8.PlayList, line)
 		default:
 			continue
-
 		}
 	}
+	sum := 0.0
+	for i := 0; i < len(m3u8.TimeList); i++ {
+		sum = sum + m3u8.TimeList[i]
+	}
+	if m3u8.Key.METHOD == "" {
+		m3u8.Key.METHOD = "NONE"
+		m3u8.Key.URI = ""
+		m3u8.Key.IV = ""
+	}
+	// fmt.Println(sum)
+	m3u8.TimeLength = sum
 	//	fmt.Println(lines)
-
 	return m3u8, nil
 
+}
+func ParseLineParameters(line string) map[string]string {
+	r := linePattern.FindAllStringSubmatch(line, -1)
+	params := make(map[string]string)
+	for _, arr := range r {
+		params[arr[1]] = strings.Trim(arr[2], "\"")
+	}
+	//	fmt.Println(params)
+	return params
 }
